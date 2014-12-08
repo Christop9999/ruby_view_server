1218286ae68259912b3fde07e290d8e6bcf26554
Chris Murolo <crmurolo@gmail.com>
2014-12-08 15:57:22 -0800

    finished

--- server_simple.rb
+++ server_simple.rb
@@ -4,11 +4,17 @@ require 'webrick'
 ## Set the path to serve files from to the public directory in this project
 root = File.expand_path 'public'
 
+
+
 # Create a new server on port 8000
 server = WEBrick::HTTPServer.new :Port => 8000, :DocumentRoot => root
 
 ## Capture control+c to shut down the server
 trap 'INT' do server.shutdown end
+
+	server.mount_proc '/' do |req, res|
+      res.body = 'Hello, world!'
+    end
 
 ## Start the server
 server.start
